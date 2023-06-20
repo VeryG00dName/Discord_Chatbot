125998e8a484de91b23022fa46a44f5a5d3d24a9
VeryGoodName <verygoodnamework@gmail.com>
2023-06-20 01:28:53 -0700

    Utilizes open ai's new function calling system

--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import openai
 import discord
 import os
+import json
 from discord.ext import commands
 
 load_dotenv()
@@ -12,17 +13,84 @@ intents.message_content = True
 openai.api_key = os.environ.get("openai-api")
 discord_token = os.environ.get("discord-token")
 global pre_prompt
-pre_prompt = ""
+global pre_pre_prompt
+pre_pre_prompt = ""
+pre_prompt = "You are a discord bot, there are lots of people in the server don't assume everyone is talking to you aspectly if they don't adress it to you"
+
+totalk=[
+    {
+        "name": "should_i_talk",
+        "description": "tell it whether in the most resent messages if someone is talking to you and if it makes sense for you to talk or not",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "answer": {
+                    "type": "string",
+                    "description": "yes or no",
+                },
+            },
+            "required": ["answer"],
+        },
+    }
+]
+
+functions=[
+    {
+        "name": "set_pre_promp",
+        "description": "sets your furture pre prompt",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "new_pre_prompt": {
+                    "type": "string",
+                    "description": "the new pre prompt",
+                },
+            },
+            "required": ["new_pre_prompt"],
+        },
+    },
+    {
+        "name": "art",
+        "description": "generates an image with a prompt",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "art_prompt": {
+                    "type": "string",
+                    "description": "prompt to generate art with",
+                },
+            },
+            "required": ["art_prompt"],
+        },
+    }
+]
+
+async def set_pre_promp(pre_prompt):
+        global pre_pre_prompt
+        pre_pre_prompt = pre_prompt
+        await bot.change_presence(activity=discord.Game(name=pre_pre_prompt))
+        
+async def art(art_prompt,message):
+        # Generate the image
+        image = openai.Image.create(
+	  	prompt=art_prompt,
+	    n=2,
+        size="512x512"
+        )
+        # Send the image to the user
+        await message.channel.send(image["data"][0]["url"])
 
 class ChatGPTBot(commands.Bot):
     async def on_message(self, message):
         global pre_prompt
+        global pre_pre_prompt
         # Handle incoming messages here
         print(message.author,message.channel,message.content)
         if message.author == self.user:
             return
         
-        if message.content.startswith("!bot"):
+        else:
+            print('test')
             # This is a message intended for the bot, so we can respond
             # Retrieve the previous messages in the current channel
             channel = message.channel
@@ -33,51 +101,56 @@ class ChatGPTBot(commands.Bot):
                 if previous_message.content == "context block":
                     break
                 if previous_message.author != self.user:
-                    dirty_user_input = previous_message.content.split()
-                    clean_user_input = ' '.join(dirty_user_input[1:])
-                    message_history.append({"role": "user", "content": str(previous_message.author) + ": " + clean_user_input})
+                    message_history.append({"role": "user", "content": str(previous_message.author) + " said " + previous_message.content})
                 else:
                     message_history.append({"role": "assistant", "content": previous_message.content})
-            message_history.reverse()
-            if pre_prompt != "":
-                message_history.append({"role": "system", "content": pre_prompt})
+            # message_history.reverse()
+            if pre_pre_prompt != "":
+                message_history.append({"role": "system", "content": pre_pre_prompt})
+            message_history.append({"role": "system", "content": pre_prompt})
             # Use the message history as input for the chatbot
+            message_history.reverse()
+            print(message_history)
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=message_history
-             )
+                model="gpt-3.5-turbo-0613",
+                messages=message_history,
+                functions=totalk,
+                function_call={"name": "should_i_talk"}
+            )
 
-            # Check if the chatbot's response is not empty or whitespace-only
-            if response.choices[0].message.content.strip():
-                # Send the chatbot's response
-                await message.channel.send(response.choices[0].message.content)
-            else:
-                # The chatbot's response is empty or whitespace-only, so we don't send it
-                return
-        elif message.content.startswith("!pre"):
-            words = message.content.split()
-            pre_prompt = ' '.join(words[1:])
-            await bot.change_presence(activity=discord.Game(name=pre_prompt))
-            return
-        elif message.content.startswith("!art"):
-       # This is a message requesting an image, so we generate one using the stable diffusion model
+            response_message = response["choices"][0]["message"]
+            print(response)
+            # Check if GPT wanted to call a function
+            
+            response_args = json.loads(response_message["function_call"]["arguments"])
+            bot_answer=response_args.get("answer")
 
-            # Generate the image
-            words = message.content.split()
-            new_prompt = ' '.join(words[1:])
-            print(new_prompt)
-            image = openai.Image.create(
-	  		    prompt=new_prompt,
-	            n=2,
-	            size="1024x1024"
-		    )
+            if bot_answer == "yes":
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo-0613",
+                    messages=message_history,
+                    functions=functions,
+                    function_call="auto"
+                )
 
-            # Send the image to the user
-            await message.channel.send(image["data"][0]["url"])
-        else:
-            # This is not a message intended for the bot, so we ignore it
-            # await message.channel.send("please start your message with !bot or !art Thank you.")
-            return
+                response_message = response["choices"][0]["message"]
+                print(response_message.content)
 
+                if response_message.get("function_call"):
+                    available_functions = {
+                    "set_pre_promp": set_pre_promp,
+                    "art": art,
+                    }
+                    function_name = response_message["function_call"]["name"]
+                    fuction_to_call = available_functions[function_name]
+                    function_args = json.loads(response_message["function_call"]["arguments"])
+                    
+                    if fuction_to_call == "set_pre_promp":
+                        fuction_to_call(pre_prompt=function_args.get("new_pre_prompt"),message=message)
+                    elif fuction_to_call == "art":    
+                            fuction_to_call(art_prompt=function_args.get("art_prompt"),message=message)
+                else:
+                    await message.channel.send(response.choices[0].message.content) 
+            
 bot = ChatGPTBot("!",intents=intents)
 bot.run(discord_token)
